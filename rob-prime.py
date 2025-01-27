658bcfd07f65d2cd6db63c29bde6695b5c9c1c0e
Rob Orange <rob.orange@niko.nz>
2025-01-28 10:49:28 +1300

    First go at searching for 100000  primes

--- rob-prime.py
+++ rob-prime.py
@@ -3,7 +3,7 @@ start = datetime.now()
 
 # Find and print the first 1,000,000 prime numbers
 primes = [2]
-
+printprime = False
 def isprime(n):
     if n % 2 == 0:
         return False
@@ -14,8 +14,13 @@ def isprime(n):
 
 n = 2
 while len(primes) < 1000000:
-    isprime(n)
+    if isprime(n):
+        primes.append(n)
+        printprime = True
     n += 1
+    if len(primes) % 10000 == 0 and printprime:
+        print(primes[-1])
+        printprime = False
 
 end = datetime.now()
 print(f"It took {end-start}")
